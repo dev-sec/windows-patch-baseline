82d3d6adfc5e16660eef8f0a2fa64ae953ec5a93
Christoph Hartmann <chris@lollyrock.com>
2016-08-23 21:43:24 +0200

    have static set of controls

--- controls/patches.rb
+++ controls/patches.rb
@@ -6,46 +6,49 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-wu = windows_update
+# cache result set
+win_update = windows_update
 
 control 'verify-kb' do
-  impact 1.0
+  impact 0.3
   title "All updates should be installed"
-  describe wu.all.length do
+  describe win_update.all.length do
     it { should eq 0}
   end
 end
 
-control 'verify-important' do
+control 'important-count' do
+  impact 1.0
+  title "No important updates should be available"
+  describe win_update.important.length do
+    it { should eq 0}
+  end
+end
+
+control 'important-patches' do
   impact 1.0
   title "All important updates are installed"
-  describe wu.important.length do
+  win_update.important.each { |update|
+    describe update do
+      it { should be_installed }
+    end
+  }
+end
+
+control 'optional-count' do
+  impact 0.3
+  title "No optional updates should be available"
+  describe win_update.optional.length do
     it { should eq 0}
   end
 end
 
-control 'verify-optional' do
+control 'optional-patches' do
   impact 0.3
   title "All optional updates are installed"
-  describe wu.optional.length do
-    it { should eq 0}
-  end
+  win_update.optional.each { |update|
+    describe update do
+      it { should be_installed }
+    end
+  }
 end
-
-wu.important.each { |update|
-  control "#{ update.title }" do
-    impact update.criticality
-    describe update do
-      it { should be_installed }
-    end
-  end
-}
-
-wu.optional.each { |update|
-  control "#{ update.title }" do
-    impact update.criticality
-    describe update do
-      it { should be_installed }
-    end
-  end
-}
